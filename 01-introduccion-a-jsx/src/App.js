08ed727b2b496aa8d5eb517227ef5d2e7339870b
César Aparicio <clark012389@hotmail.com>
2019-08-05 18:04:41 -0500

    Terminando componente con JSX

--- 01-introduccion-a-jsx/src/App.js
+++ 01-introduccion-a-jsx/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const persona = {
+  nombre: "Billy",
+  apellido: "Rupertillo",
+  edad: 50,
+  jubilacion: 60,
+  foto: "http://cort.as/-MTaG"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={persona.foto} alt="foto" />
+      <h1>{`${persona.nombre} ${persona.apellido}`}</h1>
+      <p>{persona.edad} años</p>
+      <p>Tiempo para jubilarse {persona.jubilacion - persona.edad} años 😱</p>
+      <p>{persona.edad === 60 ? <strong>✅Listo para jubilarse </strong> : <strong>😥Trabajale como esclavo😫</strong>}</p>
     </div>
   );
 }
