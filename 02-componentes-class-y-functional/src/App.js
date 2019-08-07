40ac8101060235bf13a51847e18955208855087f
César Aparicio <clark012389@hotmail.com>
2019-08-07 02:24:53 -0500

    Agregando anotaciones al componente App

--- 02-componentes-class-y-functional/src/App.js
+++ 02-componentes-class-y-functional/src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 /**
  * Class Component
  */
-// class App extends Component {
-//   render() {
-//     return (
+// class App extends Component { // Eetends from the component class
+//   render() { // The render method is mandatory
+//     return ( // And the return
 //       <div className="App">
 //         <h1>Class Component vs Functional Component</h1>
 //       </div>
@@ -18,12 +18,12 @@ import "./App.css";
 /**
  * Functional Component
  */
-function App() {
-  return (
+function App() { // It is only declared as a function or function expression
+  return ( // Use the return method
     <div className="App">
       <h1>Class Component vs Functional Component</h1>
     </div>
   );
 }
 
-export default App;
+export default App; // In both cases they are exported by name
