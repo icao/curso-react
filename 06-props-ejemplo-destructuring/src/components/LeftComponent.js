0c498c6a90ee5a60fe06f783c0f94b795603952f
César Aparicio <clark012389@hotmail.com>
2019-08-24 21:56:08 -0500

    Agregando componentes basicos

---
+++ 06-props-ejemplo-destructuring/src/components/LeftComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftComponent = () => {
+    return (
+      <section className="Container-component-left">
+        <h1>Componente Izquierda</h1>
+      </section>
+    );
+}
+
+export default LeftComponent;
---
+++ 06-props-ejemplo-destructuring/src/components/RightComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RightComponent() {
+    return (
+      <section className="Container-component-right">
+        <h1>Componente Derecho</h1>
+      </section>
+    );
+}
+
+export default RightComponent;
