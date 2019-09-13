a105ec48cd49b7e45344486d65330c6440c74cb3
César Aparicio <clark012389@hotmail.com>
2019-09-13 03:35:19 -0500

    Creación de componente para explicar asincronia

--- 14-state-que-es-setstate/src/App.js
+++ 14-state-que-es-setstate/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SetStateComponent from "./components/SetStateComponent";
+import SetStateAsynchronous from "./components/SetStateAsynchronous";
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
       <section className="App-components">
         {/* Qué es setState() */}
         <SetStateComponent />
-        {/* Ejemplo con setState y asincronía */}
-        
+        {/* Ejemplo con setState() y asincronía */}
+        <SetStateAsynchronous />
       </section>
     </div>
   );
---
+++ 14-state-que-es-setstate/src/components/SetStateAsynchronous.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SetStateAsynchronous extends Component{
+  render() {
+    return (
+      <section className="App-container-component">
+        <h1>TODO: Aqui explicacion y ejemplo de actualizar estado de la manera correcta y asincronia</h1>
+      </section>
+    );
+  }
+}
+
+export default SetStateAsynchronous;
