5a9bd958a7d1ac6ac1c84019ed2089fbbd942434
César Aparicio <clark012389@hotmail.com>
2019-09-13 03:19:30 -0500

    Agregando ejemplo más  un poco de teoría de setState y diferencia entre state y props

---
+++ 14-state-que-es-setstate/src/components/SetStateComponent.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class SetStateComponent extends Component {
+  state = {
+    contador: 0
+  };
+
+  constructor() {
+    super();
+    setInterval(() => {
+      this.setState({
+        contador: this.state.contador + 1
+      });
+    }, 1000);
+  }
+
+  render() {
+    console.log('Contador: ', this.state.contador);
+    return (
+      <section className="App-container-component">
+        <h1>Counter: {this.state.contador}</h1>
+      </section>
+    );
+  }
+}
+
+export default SetStateComponent;
+
+/** 
+ * NOTA: 
+ * Para garantizamos la reactividad en nuestra UI o dicho en otras palabras
+ * que cambien las propiedades que alimentan a nuestro componente y se vea reflejado en nuestra UI, 
+ * tenemos que hacer uso de una "propiedad dinámica" llamada estado.
+ * Pero esto no basta para garantizar la reactividad, si queremos actualizar el estado tenemos que
+ * hacer uso del método setState().
+ * 
+ * 
+ * ----------------------------------------
+ * QUÉ ES setState()?
+ * ----------------------------------------
+ * 
+ * setState() es un método que programa una actualización al 'objeto state(estado)' de un componente. 
+ * Cuando el estado cambia, el componente responde volviendo a renderizar.
+ * 
+ * setState() hace cambios al estado del componente y le dice a React que este componente 
+ * y sus elementos secundarios deben volverse a procesar con el estado actualizado. 
+ * Este es el método principal que utiliza para actualizar la interfaz de usuario en
+ * respuesta a los manejadores de eventos y las respuestas del servidor.
+ * 
+ * 
+ * ----------------------------------------
+ * DIFERENCIA ENTRE 'state' Y 'props'
+ * ----------------------------------------
+ * 
+ * 'props' y 'state' son OBJETOS PLANOS de JavsScript. Mientras ambos contienen
+ * información que influye en el resultado del render, son diferentes debido a 
+ * una importante razón:
+ *  * 'props':
+ *    - Se pasa al componente(similar a los parámetros de una función).
+ *    - Son usados para pasar dato de componentes padres a subcomponentes(hijos).
+ *    - Son inmutables(no deben cambiar).
+ *  * 'state':
+ *    - Se administra dentro del componente(similar a las variables declaradas dentro de una función).
+ *    - Son usados para datos cambiantes(dinámicos).
+ *    - Los componentes vuelven a llamar al render cuando este objeto cambia.
+ * 
+ * Otras comparaciones son:
+ * __________________________________________________________________________________________
+ *                                                                          | props | state |
+ * ¿Se puede obtener el valor inicial del componente principal?             |   Sí  |   Sí  |
+ * ¿Se puede cambiar por el componente principal?                           |   Si  |   No  |
+ * ¿Se pueden establecer valores predeterminados dentro de Componente?      |   Sí  |   Sí  |
+ * ¿Se Puede cambiar dentro del componente?                                 |   No  |   Sí  | 
+ * ¿Se puede establecer el valor inicial para los componentes secundarios?  |   Sí  |   Sí  |
+ * ¿Se Puede cambiar en componentes secundarios?                            |   Si  |   No  |
+ * __________________________________________________________________________________________
+*/
