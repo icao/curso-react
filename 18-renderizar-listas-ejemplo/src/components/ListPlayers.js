b06f2908e5ed1819889e2ec4f3f2fb5f1423d433
César Aparicio <clark012389@hotmail.com>
2019-09-17 21:25:26 -0500

    Creacion de proyecto para ejemplo de renderizado de listas

---
+++ 18-renderizar-listas-ejemplo/src/components/ListPlayers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ListPlayers extends Component {
+  render() {
+    return (
+      <section className="App-container-component">
+        <h2>Lista de jugadores</h2>
+      </section>
+    );
+  }
+}
+
+export default ListPlayers;
