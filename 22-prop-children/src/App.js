07d938dd89ddfedad6fbf620cdc27518eec70ee3
César Aparicio <clark012389@hotmail.com>
2019-09-30 03:08:18 -0500

    Creación del proyecto para prop children

---
+++ 22-prop-children/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <section className="App-components">
+        <h1>Propiedad Children</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
