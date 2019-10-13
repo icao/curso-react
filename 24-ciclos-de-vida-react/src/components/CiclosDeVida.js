8c08cea73aac3af37b326da5d5c09065a48be960
César Aparicio <clark012389@hotmail.com>
2019-10-12 22:55:33 -0500

    Corrigiendo ciclos de vida deprecados

--- 24-ciclos-de-vida-react/src/components/CiclosDeVida.js
+++ 24-ciclos-de-vida-react/src/components/CiclosDeVida.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class CiclosDeVida extends Component{
+class CiclosDeVida extends Component {
   render() {
     return (
       <section className="App-container-component-2">
@@ -25,9 +25,26 @@ class CiclosDeVida extends Component{
           <li>Obtiene las props</li>
           <li>Bindeamos métodos de clase(bind de los handles)</li>
           <li>
-            Primera ejecución del método render() ,' una vez ejecutado ya
+            Primera ejecución del método render() -> una vez ejecutado ya
             podremos visualizarlo en el DOM
           </li>
+          <li>
+            Métodos en la fase de montaje:
+            <ul>
+              <li>constructor()</li>
+              <li>ComponentWillMount -> static getDerivedStateFromProps()</li>
+              <li>render()</li>
+              <li>componentDidMount()</li>
+            </ul>
+          </li>
+          <p>
+            <strong>Nota:</strong> Estos métodos están considerados legacy
+            (deprecados) y debes evitarlos en código nuevo:
+            <ul>
+              <li>UNSAFE_componentWillMount()</li>
+            </ul>
+          </p>
+          <p>A los metodos deprecados se les antepone la palabra UNSAFE</p>
         </ul>
         <br />
         <h2>Actualización</h2>
@@ -41,19 +58,60 @@ class CiclosDeVida extends Component{
             nuevo para reflejar los cambios
           </li>
           <li>Si no lo hacemos el componente se rendizará de nuevo</li>
+          <li>
+            Métodos en la fase de Actualización
+            <ul>
+              <li>
+                ComponentWillReceiveProps -> static getDerivedStateFromProps()
+              </li>
+              <li>shouldComponentUpdate()</li>
+              <li>render()</li>
+              <li>ComponentWillUpdate -> getSnapshotBeforeUpdate()</li>
+              <li>componentDidUpdate()</li>
+            </ul>
+          </li>
+          <p>
+            <strong>Nota:</strong> Estos métodos están considerados legacy
+            (deprecados) y debes evitarlos en código nuevo:
+            <ul>
+              <li>UNSAFE_componentWillUpdate()</li>
+              <li>UNSAFE_componentWillReceiveProps()</li>
+            </ul>
+          </p>
         </ul>
+
         <br />
         <h2>Desmontaje</h2>
         <ul>
           <li>Eliminamos listeners</li>
           <li>Eliminamos referencias al DOM</li>
+          <li>
+            Método en la fase de Desmontaje
+            <ul>
+              <li>componentWillUnmount()</li>
+            </ul>
+          </li>
         </ul>
         <br />
-        <hr/>
-        <p>NOTA: Cada una de estas fases se compone por diferentes métodos que se autoejecutan, los cuales nos permitiran llevar a cabo ciertas actividades que querramos llevar a cabo.</p>
+        <hr />
+        <p>
+          NOTA: Cada una de estas fases se compone por diferentes métodos que se
+          autoejecutan, los cuales nos permitiran llevar a cabo ciertas
+          actividades que querramos llevar a cabo. <br /> Adicional existen
+          métodos para el manejo de errores llamados:
+          <strong>
+            <ul>
+              <li>static getDerivedStateFromError()</li>
+              <li>componentDidCatch()</li>
+            </ul>
+          </strong>
+          Estos métodos se invocan cuando hay un error durante la renderización,
+          en un método en el ciclo de vida o en el constructor de cualquier
+          componente hijo.
+        </p>
       </section>
     );
   }
 }
 
-export default CiclosDeVida;  
+export default CiclosDeVida;
