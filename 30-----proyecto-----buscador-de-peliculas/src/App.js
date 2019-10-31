48458e90ba078893e5e4246599969c51261ddc58
César Aparicio <clark012389@hotmail.com>
2019-10-30 22:03:50 -0600

    Creando proyecto buscador de peliculas

---
+++ 30-----proyecto-----buscador-de-peliculas/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.css'
+import {Title} from './components/Title'
+
+function App() {
+  return (
+    <div className="container">
+      <Title>Buscador de peliculas</Title>
+    </div>
+  );
+}
+
+export default App;
---
+++ 30-----proyecto-----buscador-de-peliculas/src/components/Title.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Title = ({ children }) => (
+  <h1 class="title is-1 text-center">{children}</h1>
+);
