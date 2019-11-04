42b88a9063d82e530dbe8a3b59067e8e9a4c360a
César Aparicio <clark012389@hotmail.com>
2019-11-03 23:07:42 -0600

    Terminando ejemplo de comunicación entre componentes de hijo a padre

--- 31-comunicacion-entre-componentes/src/components/ChildTwo.js
+++ 31-comunicacion-entre-componentes/src/components/ChildTwo.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 
 class ChildTwo extends Component {
-  state = { counter: 0 };
+  state = { counter: 0 }; //Inicializamos contador hijo
 
-  handleClick = () => {
+  handleClick = () => { //Método para incrementar el contador del hijo y lanzar el método para incrementar en el padre
     this.setState(state => {
       return {
         counter: state.counter + 1
       };
     });
 
-    this.props.increment();
+    this.props.increment(); // Lanzamos el evento que es pasado desde el padre, atra vez de las props.
   };
 
   render() {
@@ -20,6 +20,7 @@ class ChildTwo extends Component {
         <p>
           Contador: <strong>{this.state.counter}</strong>
         </p>
+        {/* Mandamos a llamar a lmétodo que incrementará los contadores */}
         <button onClick={this.handleClick}>Incrementar</button>
         {/* Otra forma mas simple es teniendo el botón y solo diparar el evento para que se incrmente el contador en el padre */}
         {/* <button onClick={this.props.increment}>Incrementar</button> */}
@@ -29,3 +30,9 @@ class ChildTwo extends Component {
 }
 
 export default ChildTwo;
+
+/** 
+ * En este caso el hijo tendra su propio contador,
+ * cuando este incremente, emitira el evento al padre, haciendo que
+ * su contador incremente tambien.
+*/
--- 31-comunicacion-entre-componentes/src/components/ParentTwo.js
+++ 31-comunicacion-entre-componentes/src/components/ParentTwo.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import ChildTwo from "./ChildTwo";
 
 class ParentTwo extends Component {
-  state = { counter: 0 };
+  state = { counter: 0 };//Inicializamos contador padre
 
-  handleSubmit = () => {
+  handleSubmit = () => { //Método que es ejecutado en el padre para incrementar el contador
     this.setState(state => {
       return {
         counter: state.counter + 1
@@ -20,6 +20,7 @@ class ParentTwo extends Component {
         <p>
           Contador: <strong>{this.state.counter}</strong>
         </p>
+        {/* Para comunicarnos con el hijo, pasamos la función del padre atravez de una prop al hijo, que será ejecutada en un futuro desde el hijo */}
         <ChildTwo increment={this.handleSubmit} />
       </section>
     );
@@ -27,3 +28,16 @@ class ParentTwo extends Component {
 }
 
 export default ParentTwo;
+
+/** 
+ * ----------------------------------------------
+ * Comunicar componentes de 👦Hijo ➡ 👨Padre
+ * ----------------------------------------------
+ * Para comunicar un hijo a un padre, tenemos que crear un enlace del hijo al padre. Esto lo hacemos haciendo uso de una función que sera pasada a travez de una prop del padre al hijo. 
+ * 
+ * El hijo cuando quiera comunicarse con el padre, hará uso de esta función pasada por el padre. 
+ * Esta funcion llega al hijo mediante una prop. Por lo que para ejecutarla
+ * solo mandaremos a llamar a la prop, ya sea ejecutandola capturada en un evento por ejemplo OnClick, o mandandola a ejecutar mediante el uso de () en otro método del componente hijo, como se muestra en el ejemplo. 
+ * 
+ * 
+*/
