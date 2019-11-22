5a1dd9148c81802e14aa013f7d9be2d2b4e5165d
César Aparicio <clark012389@hotmail.com>
2019-11-22 16:35:33 -0600

    Separando App de Home para envolver a la app

---
+++ 33-----proyecto-----buscador-de-peliculas-v2/src/pages/DetailMovie.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DetailMovie extends Component {
+  render() {
+    return <h1>Este es el detalle de la pelicula</h1>;
+  }
+}
+
+export default DetailMovie;
