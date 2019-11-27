69e3a4778ff6dd14ab4455fe542b14088c2f4edc
César Aparicio <clark012389@hotmail.com>
2019-11-27 16:44:13 -0600

    Agregando proyecto de uso de svgs en react js

---
+++ 34-svg-en-react/src/components/SpinnerLoader.js
@@ -0,0 +1,202 @@
+import React from "react";
+
+export const SpinnerLoader = () => {
+  const SVG = {
+    strokeMiterlimit: "10",
+    strokeWidth: "3.5"
+  };
+  return (
+    <div className="container__loader">
+      <div className="icon-loader">
+        <svg
+          viewBox="0 0 100 100"
+          y="0"
+          x="0"
+          xmlns="http://www.w3.org/2000/svg"
+          id="Layer_1"
+          version="1.1"
+          style={{
+            height: `100%`,
+            width: `100%`,
+            background: `rgba(255, 255, 255, 0) none repeat scroll 0% 0%`
+          }}
+          width="186px"
+          height="186px"
+        >
+          <g style={{ transformOrigin: `50% 50% 0px`, transform: `scale(1)` }}>
+            <g style={{ transformOrigin: `50px 50px 0px` }}>
+              <g>
+                <g
+                  style={{
+                    transformOrigin: `50px 50px 0px`,
+                    transform: `matrix(1, 0, 0, 1, 0, 0)`,
+                    animation: `1.42857s linear -1.07857s infinite normal forwards running tremble`
+                  }}
+                >
+                  <g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#f4e6c8"
+                          d="M83.9 26.8c0-2-1.3-3.7-3-4.4-.7-2.2-2.5-4-4.8-4.5-.5-3.1-3.2-5.4-6.4-5.4-2.9 0-5.4 1.9-6.3 4.6-.8-.3-1.6-.5-2.5-.5-.6 0-1.2.1-1.8.3-1.1-2-3.3-3.4-5.8-3.4-.8 0-1.5.1-2.2.4-1-2.2-3.3-3.8-5.9-3.8-3.5 0-6.3 2.7-6.5 6.1-.9-.4-1.9-.7-2.9-.7-.6 0-1.1.1-1.6.2-1.2-1.4-3-2.3-4.9-2.3-3.6 0-6.6 2.9-6.6 6.6 0 .3 0 .7.1 1-1.4 0-2.7.5-3.8 1.3-1.6 1.2-2.7 3.1-2.7 5.3 0 2.2 1 4.1 2.7 5.3.7.5 1.6.9 2.5 1.1h59.1c.4-.8.7-1.8.8-2.8 1.3-.8 2.5-2.5 2.5-4.4z"
+                          style={{
+                            fill: `rgb(244, 230, 200)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="none"
+                          d="M42.6 29.4c-1.9-1.9-1.8-5.5.2-7.3s5.6-1.5 7.3.6c1.2 1.5 1.1 4.1-.3 5.5"
+                          style={{ stroke: `rgb(51, 51, 51)` }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="none"
+                          d="M51 25c.9-1.5 2.8-2.2 4.5-1.7 1.6.5 2.9 2.2 2.8 3.9"
+                          style={{ stroke: `rgb(51, 51, 51)` }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#f4e6c8"
+                          d="M76 17.9c-.9 0-7.3-.5-7.2 6"
+                          style={{
+                            fill: `rgb(244, 230, 200)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+              <g>
+                <g
+                  style={{
+                    transformOrigin: `50px 50px 0px`,
+                    transform: `matrix(1, 0, 0, 1, 0, 0)`,
+                    animation: `1.42857s linear -1.42857s infinite normal forwards running tremble`
+                  }}
+                >
+                  <g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#e15c64"
+                          d="M36.6 90h-7c-1.5 0-2.7-1.1-2.9-2.6l-5.5-53.6h11.9L36.6 90z"
+                          style={{
+                            fill: `rgb(235, 65, 116)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          d="M46.2 90h-9.6l-3.4-56.2H45z"
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#fff"
+                          style={{
+                            fill: `rgb(255, 255, 255)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          d="M55.8 90h-9.6L45 33.8h11.9z"
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#e15c64"
+                          style={{
+                            fill: `rgb(235, 65, 116)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          d="M65.3 90h-9.5l1.1-56.2h11.9z"
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#fff"
+                          style={{
+                            fill: `rgb(255, 255, 255)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#e15c64"
+                          d="M72.3 90h-7l3.4-56.2h11.9l-5.5 53.6c-.1 1.5-1.3 2.6-2.8 2.6z"
+                          style={{
+                            fill: `rgb(235, 65, 116)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                    <g>
+                      <g style={{ transformOrigin: `50px 50px 0px` }}>
+                        <path
+                          strokeMiterlimit={SVG.strokeMiterlimit}
+                          strokeWidth={SVG.strokeWidth}
+                          stroke="#333"
+                          fill="#f7b26a"
+                          d="M56.9 66.7h-12c-2.8 0-5-2.2-5-5v-4.5c0-2.8 2.2-5 5-5h12c2.8 0 5 2.2 5 5v4.5c0 2.8-2.2 5-5 5z"
+                          style={{
+                            fill: `rgb(248, 172, 92)`,
+                            stroke: `rgb(51, 51, 51)`
+                          }}
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+};
