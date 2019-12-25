aea150c74ee4b7ef8ead6fb64873ab10f9666707
César Aparicio <clark012389@hotmail.com>
2019-12-25 01:29:27 -0600

    cambiando estilos del slogan del header

--- 36-----proyecto-----buscador-de-noticias/src/components/Header.js
+++ 36-----proyecto-----buscador-de-noticias/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ title }) => {
   return (
     <div className="header__content">
       <h1 className="header__title__newspaper">{title}</h1>
-      <h3 className="header__subtitle__newspaper">the modern newspaper</h3>
+      <p className="header__subtitle__newspaper">the modern newspaper</p>
     </div>
   );
 };
