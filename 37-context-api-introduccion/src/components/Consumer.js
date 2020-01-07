ed4cc47fdffca2aa9b6eeb5a06ee4496495c2f0d
César Aparicio <clark012389@hotmail.com>
2020-01-07 16:56:13 -0600

    Terminado documentacion de Consumidor

--- 37-context-api-introduccion/src/components/Consumer.js
+++ 37-context-api-introduccion/src/components/Consumer.js
@@ -5,3 +5,25 @@ const Consumer = () => {
 }
  
 export default Consumer;
+
+/** 
+ * ***********************
+ * Context.Consumer
+ * ***********************
+*/
+
+/*
+  Un componente de React que se suscribe a cambios de contexto.
+
+  <MiContexto.Consumer>
+    {
+      value => {//Renderiza algo basado en el valor dle contexto}
+    }
+  <MiContexto.Consumer />
+
+  
+  Esto le permite suscribirse a un contexto dentro de un componente de función. 
+  Requiere una función como hijo. La función recibe el valor de contexto actual y devuelve un nodo de React. El argumento 'value' pasado a la función será igual al prop 'value' del 'Provider' MAS CERCANO para este contexto e el árbol. 
+  Si no hay un 'Proveedor' superior para este contexto, el argumento 'value' será igual al 'defaultValue' que se pasó a createContext();
+
+*/
