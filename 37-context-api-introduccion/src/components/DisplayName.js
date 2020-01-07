4a6042db344d04ca2fb3d22b41724d909034cbca
César Aparicio <clark012389@hotmail.com>
2020-01-07 17:06:28 -0600

    Agregando documentación de context display name

--- 37-context-api-introduccion/src/components/DisplayName.js
+++ 37-context-api-introduccion/src/components/DisplayName.js
@@ -5,3 +5,24 @@ const DisplayName = () => {
 }
  
 export default DisplayName;
+
+/** 
+ * ***********************
+ * Context.displayName
+ * ***********************
+*/
+
+/*
+  El objeto Context acepta una propiedad de cadena de texto 'displayName'. Las herramientas de desarrollo de React utilizan esta cadena de texto para determinar que mostrar para el Context. 
+
+  Por ejemplo, el componente a continuación aparecerá como "NombreAMostrar" en las herramientas de desarrollo: 
+  
+  const MiContexto = React.createContext('AlgunValor)
+
+  MiContexto.displayName = "NombreAMostrar";
+
+  <MiContexto.Provide> //"NombreAMostrar.Provider" en las herramientas de desarrollo
+
+  <MiContexto.Consumer> //"NombreAMostrar.Consumer" en las herramientas de desarrollo
+    
+*/
