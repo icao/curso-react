60a164475d348122d3765ce110f96a82dbf8027d
César Aparicio <clark012389@hotmail.com>
2020-01-08 00:35:15 -0600

    Creando ejemplo con estilos en la vista

--- 38-context-api-ejemplo-01/src/App.js
+++ 38-context-api-ejemplo-01/src/App.js
@@ -1,13 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ejemplos Context API</h1>
-    </div>
-  );
+const AppContext = React.createContext({
+  usuarios: [],
+  titulo: "titulo default"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Context API</h1>
+        <div className="grandpa">
+          <h2>
+            <span role="img" aria-label="sheep">
+              👴
+            </span>
+            Abuelo
+          </h2>
+          <p className="italic">Creando Context y disponiendo el Provider</p>
+          <AppContext.Provider
+            value={{
+              usuarios: ["ramona", "billy", "rufus"],
+              titulo: "Ejemplo Context API"
+            }}
+          >
+            {/* componente hijo */}
+            <Test />
+          </AppContext.Provider>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="father">
+        <h2>
+          <span role="img" aria-label="sheep">
+            👨
+          </span>
+          Papá
+        </h2>
+        <Lista />
+      </div>
+    );
+  }
+}
+
+class Lista extends Component {
+  render() {
+    return (
+      <div className="children">
+        <h2>
+          <span role="img" aria-label="sheep">
+            👦
+          </span>
+          Hijo
+        </h2>
+        <p className="italic">Suscripción al Provider con el Consumer</p>
+        <AppContext.Consumer>
+          {value => {
+            return (
+              <ul>
+                {value.usuarios.map(usuario => (
+                  <li>{usuario}</li>
+                ))}
+              </ul>
+            );
+          }}
+        </AppContext.Consumer>
+      </div>
+    );
+  }
+}
+
+// export default Lista;
