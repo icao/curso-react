bb525f74eda3060742784d204c0c04742790d4cc
César Aparicio <clark012389@hotmail.com>
2020-01-09 00:23:13 -0600

    Agregando archivos para proyecto nuevo de buscador de eventos

---
+++ 40-----proyecto-----buscador-de-eventos-context-api/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto de buscador de eventos</h1>
+    </div>
+  );
+}
+
+export default App;
