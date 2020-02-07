acec095644aa32fb283c4e07374010d044e4afc3
César Aparicio <clark012389@hotmail.com>
2020-02-07 00:28:14 -0600

    Convertiendo app component a class component

--- 40-----proyecto-----buscador-de-eventos-context-api/src/App.js
+++ 40-----proyecto-----buscador-de-eventos-context-api/src/App.js
@@ -1,22 +1,37 @@
-import React, { Fragment } from "react";
+import React, {Component, Fragment } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import HeroBanner from "./components/HeroBanner";
 import Form from "./components/Form";
 
-function App() {
-  return (
-    <Fragment>
-      <header className="container__header">
-        <Header />
-      </header>
-      <section className="container__hero__banner">
-        <HeroBanner>
-          <Form />
-        </HeroBanner>
-      </section>
-    </Fragment>
-  );
+
+export const AppContext = React.createContext({
+  categories: []
+})
+class App extends Component {
+  componentDidMount() {
+    // hacer peticion al servicio para obtener las categorias
+    // una vez obtenida las categorias, enviar al estado
+
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {/* una vez obtenido las categorias, pasarlo como value al provider */}
+        <AppContext.Provider> 
+          <header className="container__header">
+            <Header />
+          </header>
+          <section className="container__hero__banner">
+            <HeroBanner>
+              <Form />
+            </HeroBanner>
+          </section>
+        </AppContext.Provider>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
