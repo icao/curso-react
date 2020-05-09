364162d8eb5f8b3dbcb7abffc6046ec2cd102beb
César Aparicio <clark012389@hotmail.com>
2020-05-09 05:31:23 -0500

    Archivos base para seccion de el hook usestate

---
+++ 42-hook-use-state/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseState from './components/useState/UseState'
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+    </div>
+  );
+}
+
+export default App;
---
+++ 42-hook-use-state/src/components/useState/UseState.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UseState = () => {
+  return <div>
+    <h1>useState Hook</h1>
+  </div>
+}
+ 
+export default UseState;
