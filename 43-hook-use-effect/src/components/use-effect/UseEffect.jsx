7007c2f7f6da50c3c5eda52aa6071a72d7e90ad3
César Aparicio <clark012389@hotmail.com>
2020-05-16 04:42:58 -0500

    Agregando proyecto para el hook de use effect

---
+++ 43-hook-use-effect/src/components/use-effect/UseEffect.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const UseEffect = () => {
+  return (
+    <div>
+      <h1>
+        <span role='img' aria-label='jsx-a11y/accessible-emoji'>
+          ⚓
+        </span>{' '}
+        Hook useEffect
+      </h1>
+    </div>
+  )
+}
+
+export default UseEffect
