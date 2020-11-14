424edb2b6587b8fcde843f108c53a2d96911a3bf
César Aparicio <clark012389@hotmail.com>
2020-11-14 01:43:05 -0600

    Implementacion final de todos los ejemplos con useEffect

--- 43-hook-use-effect/src/components/example-use-effect/ExampleUseEffect.jsx
+++ 43-hook-use-effect/src/components/example-use-effect/ExampleUseEffect.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const ExampleUseEffect = () => {
+  const [mostrarComponente, setMostrarComponente] = useState(true)
   return (
     <>
       <h1>Ejemplo Formulario - useEffect</h1>
@@ -14,9 +15,14 @@ const ExampleUseEffect = () => {
         3- useEffect SE EJECUTA MONITOREANDO PARAMETROS EN EL ARRAY -{' '}
         <em>componentDidUpdate</em>
       </h3>
+      <TercerEjemplo />
       <h3>
         4.- useEffect SANEA UNA SUSCRIPCION - <em>componentWillUnmount</em>
       </h3>
+      <button onClick={() => setMostrarComponente(!mostrarComponente)}>
+        {mostrarComponente ? `Ocultar` : `Mostrar`}
+      </button>
+      {mostrarComponente && <EjemploCuarto />}
       <h4>
         <span role='img' aria-label='jsx-a11y/aria-proptypes'>
           🚧{' '}
@@ -32,6 +38,7 @@ const PrimerEjemplo = () => {
   const [nombre, setNombre] = useState('John')
 
   useEffect(() => {
+    // El useEffect se ejecuta siempre que cambie el estado, de cualquier estado, pueden ser varios
     console.log(
       `%cEj1. useEffect -> Estoy montado y me ejecuto siempre que me renderizo`,
       'color: darkturquoise; font-weight: bold; font-size: 18px;'
@@ -67,6 +74,7 @@ const SegundoEjemplo = () => {
   const [nombre, setNombre] = useState('')
 
   useEffect(() => {
+    // El useEffect solo se ejecuta una vez, hace efecto despues de el primer render
     console.log(
       `%cEj2. useEffect -> Estoy montado y me ejecuto solo una vez`,
       'color: #3030ff; font-weight: bold; font-size: 18px;'
@@ -112,6 +120,104 @@ const SegundoEjemplo = () => {
           <em>Obteniendo Usuario...</em>
         </strong>
       )}
+    </>
+  )
+}
+
+const TercerEjemplo = () => {
+  const [nombreMascota, setNombreMascota] = useState('')
+  const [edad, setEdad] = useState(0)
+  const [raza, setRaza] = useState('')
+
+  useEffect(() => {
+    // El useEffect solo se ejecuta en el primer render y posteriormente si cambia SOLO el estado NOMBREMASCOTA
+    console.log(
+      `%cEj3. useEffect -> Estoy monitoreando, solo me ejecuto si cambia el nombre`,
+      'color: #ffac33; font-weight: bold; font-size: 18px;'
+    )
+  }, [nombreMascota])
+
+  function cambiarNombre(event) {
+    setNombreMascota(event.target.value)
+  }
+  function cambiarEdad(event) {
+    setEdad(event.target.value)
+  }
+  function cambiarRaza(event) {
+    setRaza(event.target.value)
+  }
+  return (
+    <>
+      <form>
+        <label htmlFor='id-nombre-mascota'>
+          Nombre{' '}
+          <input
+            id='id-nombre-mascota'
+            type='text'
+            value={nombreMascota}
+            onChange={cambiarNombre}
+          />
+        </label>
+        <strong>{nombreMascota}</strong>
+        <br />
+        <label htmlFor='id-edad'>
+          Edad{' '}
+          <input
+            id='id-edad'
+            type='number'
+            value={edad}
+            onChange={cambiarEdad}
+          />
+        </label>
+        <strong>{edad}</strong>
+        <br />
+        <label htmlFor='id-raza'>
+          Raza{' '}
+          <input
+            id='id-raza'
+            type='text'
+            placeholder='Chihuahua, Pitbull, Coquer, Pastor, etc'
+            value={raza}
+            onChange={cambiarRaza}
+          />
+        </label>
+        <strong>{raza}</strong>
+      </form>
+    </>
+  )
+}
+
+const EjemploCuarto = () => {
+  const [ejeX, setEjeX] = useState(0)
+  const [ejeY, setEjeY] = useState(0)
+
+  useEffect(() => {
+    console.log(
+      `%cEj4. useEffect -> Agregando event listener mouseover`,
+      'color: #F48FB1; font-weight: bold; font-size: 18px;'
+    )
+    // Suscripcion de evento mouseover
+    window.addEventListener('mouseover', capturaMovimiento)
+
+    return () => {
+      console.log(
+        `%cEj4. useEffect -> Removiendo/Limpiando/Saneando event listener mouseover`,
+        'color: #E91E63; font-weight: bold; font-size: 18px;'
+      )
+      // UseEffect cada vez que se renderiza el componente desmonta/limpia/sanea el eventListener
+      window.removeEventListener('mouseover', capturaMovimiento)
+    }
+  })
+
+  function capturaMovimiento(event) {
+    setEjeX(event.clientX)
+    setEjeY(event.clientY)
+  }
+
+  return (
+    <>
+      <h4>{ejeX}</h4>
+      <h4>{ejeY}</h4>
     </>
   )
 }
