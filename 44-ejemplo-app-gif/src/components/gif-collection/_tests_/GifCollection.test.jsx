2a1bc265778fb532cf148832b894e3f35eebbe57
César Aparicio <clark012389@hotmail.com>
2020-11-25 23:50:21 -0600

    Configurando enzyme en el proyecto de giphy app

---
+++ 44-ejemplo-app-gif/src/components/gif-collection/_tests_/GifCollection.test.jsx
@@ -0,0 +1,5 @@
+describe('Probando el componente <GifCollection />', () => {
+  test('El componente <GifCollection /> debe de renderearse correctamente', () => {
+    console.log('SERE UN TEST')
+  })
+})
--- 44-ejemplo-app-gif/src/setupTests.js
+++ 44-ejemplo-app-gif/src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom'
+// Importación de Enzyme y un adapter(no  ificial, revisar en un futuro cuando enzyme implemente react 17) para React 17
+import Enzyme from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+// Importación de plugin de enzyme para tener snapshot con que comparar
+import { createSerializer } from 'enzyme-to-json'
+
+// Configuración del adaptador para enzyme
+Enzyme.configure({ adapter: new Adapter() })
+// Configuración de plugin enzyme-to-json
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
