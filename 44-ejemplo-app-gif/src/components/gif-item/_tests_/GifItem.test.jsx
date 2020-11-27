e05fa1fec8219c7f7d7c53a22e0f68804eeed867
César Aparicio <clark012389@hotmail.com>
2020-11-27 15:01:54 -0600

    Implementacion de los test del componente GifItem

--- 44-ejemplo-app-gif/src/components/gif-item/_tests_/GifItem.test.jsx
+++ 44-ejemplo-app-gif/src/components/gif-item/_tests_/GifItem.test.jsx
@@ -3,10 +3,32 @@ import GifItem from '../GifItem'
 
 describe('Probando componente <GifItem />', () => {
   const title = 'Soy el nombre del gif'
-  const url = 'http://algo_direccion_imagen'
-  test('<GifItem /> debe mostrarse correctamente y aceptar las props: title, url', () => {
-    const wrapper = shallow(<GifItem title={title} url={url} />)
+  const url = 'http://alguna_direccion_imagen'
+  const wrapper = shallow(<GifItem title={title} url={url} />)
 
+  test('<GifItem /> debe mostrarse correctamente y aceptar las props: title, url', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  test('El texto(titulo) renderizado debe ser el mismo a la prop titulo', () => {
+    const titulo = wrapper.find('h4').text().trim()
+    expect(titulo).toBe(title)
+  })
+
+  test("El atributo 'src' debe ser igual a la prop url", () => {
+    const urlImg = wrapper.find('img').props().src
+    expect(urlImg).toBe(url)
+  })
+
+  test("El atributo 'alt' igual a la prop titulo", () => {
+    const altImg = wrapper.find('img').prop('alt')
+    expect(altImg).toBe(title)
+  })
+
+  test('El componente debe de tener la clase animate__fadeInDown', () => {
+    const classImg = wrapper.find('div').at(0).prop('className')
+    console.log(classImg)
+    // Se espera que exista la clase animate__fadeInDown en el div contenedor
+    expect(classImg.includes('animate__fadeDown')).toBe(true)
   })
 })
