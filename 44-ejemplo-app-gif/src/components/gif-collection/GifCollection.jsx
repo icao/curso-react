90970db235cbd9ddfb00aba55f909792383ffd1c
César Aparicio <clark012389@hotmail.com>
2020-12-18 03:59:51 -0600

    Implementando prop types al componente

--- 44-ejemplo-app-gif/src/components/gif-collection/GifCollection.jsx
+++ 44-ejemplo-app-gif/src/components/gif-collection/GifCollection.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import GifItem from '../gif-item/GifItem'
 import './GifCollection.scss'
 import { useFetchGifs } from '../../hooks/useFetchGifs'
@@ -8,8 +9,8 @@ const GifCollection = ({ category }) => {
 
   return (
     <>
-      {isLoading && <h2>Loading...</h2>}
       <h2 className='title-collection'>{category}</h2>
+      {isLoading && <h2>Loading...</h2>}
       <div className='container-collection'>
         {/* Pinta las imagenes obtenidas por la API */}
         {images.map(image => (
@@ -20,10 +21,14 @@ const GifCollection = ({ category }) => {
   )
 }
 
+GifCollection.propTypes = {
+  category: PropTypes.string.isRequired,
+}
+
 export default GifCollection
 
 /**
- * [1] Mandamos las propiedades a nuestro componente que pintara cada imagen de la colección, lo mandamos con el spread operator porque si recorbdamos,
+ * [1] Mandamos las propiedades a nuestro componente que pintará cada imágen de la colección, lo mandamos con el spread operator porque si recordamos,
  * equivaldría a si nosotros enviaramos de la siguiente manera:
  *
  *  <GifItem
@@ -32,11 +37,11 @@ export default GifCollection
  *    url={image.url}
  *  />
  *
- * Al mandalor como:
+ * Al mandarlo como:
  *  <GifItem
  *    key={image.id}
  *    {...image}
  *  />
  *
- * Sabemos que dentro del componente GifItem, estaremos esperando las propiedades y desde ahi podemos destructurar las props enviadas
+ * Sabemos que dentro del componente GifItem, estaremos esperando las propiedades y desde ahí podemos destructurar las props enviadas
  */
