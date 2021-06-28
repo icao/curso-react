0806beca268d9b3cac8b512dfdcac9b5647e9795
cesar-aparicio <cesar@thirty3.ch>
2021-06-28 13:51:43 -0500

    Agregando test y eliminacion de comentarios

--- 36-----proyecto-----buscador-de-noticias/src/services/serviceNewsAPI.js
+++ 36-----proyecto-----buscador-de-noticias/src/services/serviceNewsAPI.js
@@ -1,17 +1,4 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-// const qInTitle = ''
-// const from = ''
-// const apiKey = ''
-// const url = `${proxyUrl}https://newsapi.org/v2/everything?qInTitle=${qInTitle}&from=${from}language=en&apiKey=${apiKey}`
-
-// fetch(request)
-//   .then(response => response.json())
-//   .then(news => {
-//     console.log(news)
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
 
 const API_KEY = 'e1337f0a1a8b482eabec531aca4881a8'
 
